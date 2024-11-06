59296a6856665e222356a86bb1482b1049aef002
mofq786 <mofq9786@gmail.com>
2024-11-06 20:20:47 +0530

    NavBar Created

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,6 @@
 import Menu from "./Menu";
 import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 function NavBar()
 {
     const [showMenu, setShowMenu] = useState(false);  // State to control menu visibility
@@ -9,19 +10,19 @@ function NavBar()
         <>
         <nav>
             <button className="nav_button" id="logo">
-                <img src="src/assets/images/logo3.jpg" alt="" id="logo_image" />
+                <img src="src/assets/images/logo.jpg" alt="" id="logo_image" />
             </button>
-            <input type="search" name="search_box" id="search_box" placeholder="search medicines here" />
+            <input type="search" name="search_box" id="search_box" placeholder="Search Medicines Here" />
             <button className="nav_button" id="search_icon">
                 <img src="src/assets/images/magnifying-glass-solid.svg" alt="" />
             </button>
             <button className="nav_button" id="cart_icon">
                 <img src="src/assets/images/cart-shopping-solid.svg" alt="" />
-                Cart
+                <p>Cart</p>
             </button>
             <button className="nav_button" id="profile_icon" onClick={toggleMenu}>
                 <img src="src/assets/images/user-solid.svg" alt="" />
-                Profile
+                <p>Profile</p>
             </button>
             {showMenu && <Menu />}
         </nav>
